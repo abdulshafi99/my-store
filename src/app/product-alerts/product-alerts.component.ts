35e240f257e73eaa80254001e5bacbade3b47e19
abdulshafi99 <abdulshafielwahsh@gmail.com>
2025-04-23 11:00:19 +0200

    feat(product-alerts): add notification functionality to ProductAlerts component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -8,6 +8,7 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
-  
+  @Output() notifiy = new EventEmitter;
+
   
 }
